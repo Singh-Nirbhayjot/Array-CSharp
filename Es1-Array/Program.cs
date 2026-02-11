097a6793df1034547b013ee72199d2d99a537b67
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-11 19:21:31 +0100

    Aggiungere i file di progetto.

---
+++ Es1-Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+
+namespace Es1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool d = true;
+            do
+            {
+                WriteLine("Inserisci lunghezza del vettore");
+            }
+            while (!int.TryParse(ReadLine(), out n) || n <= 0);
+ 
+            int[] v = new int[n];
+            WriteLine("Inserisci valori");
+            for(int i=0; i < v.Length; i++)
+            {
+                while (!int.TryParse(ReadLine(), out v[i]))
+                {
+                    WriteLine("Inserisci valore valido");
+                }
+            }
+            for(int i=0; i < v.Length -1; i++)
+            {
+                for(int j=i+1; j < v.Length; j++)
+                {
+                    if (v[i] == v[j])
+                    {
+                        d = false;
+                        break;
+                    }
+                    
+                }
+                if (!d)
+                {
+                    break;
+                }
+            }
+            if (d)
+            {
+                Write(1);
+            }
+            else Write(0);
+
+        }
+    }
+}
