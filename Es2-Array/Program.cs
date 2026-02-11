bf245f82fb3a45bdc7beb5b9ec1623892d5800d7
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-11 19:54:20 +0100

    Aggiunti i file

---
+++ Es2-Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace Es2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[30];
+            Random rnd = new Random();
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = rnd.Next(0, 101);
+                Write(v[i] + " ");
+            }
+            Write("\n");
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] % 2 == 0)
+                {
+                    for (int j = 0; j < v.Length; j++)
+                    {
+                        if (v[i] == 2 * v[j])
+                        {
+                            Write(v[i] + " " + v[j]);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
